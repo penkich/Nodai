e58762c70b95a5ce9327b68842ff3987a2f018a6
Penkich <nishijim2001@yahoo.co.jp>
2024-12-02 18:54:21 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,7 +4,7 @@ from machine import Pin, I2C
 import ssd1306
 
 time.sleep(2)
-i2c = I2C(sda=Pin(6), scl=Pin(7))
+i2c = I2C(sda=Pin(4), scl=Pin(5))
 display = ssd1306.SSD1306_I2C(128, 64, i2c)
 
 regex = re.compile(",")
