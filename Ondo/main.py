d016aff13eb247900ef306ff8edb3926a0bc8d98
Penkich <nishijim2001@yahoo.co.jp>
2025-01-07 18:21:42 +0900

    Update main.py

--- Ondo/main.py
+++ Ondo/main.py
@@ -1,9 +1,9 @@
 """
+GASに送信して、ワークシートに温湿度センサーの値を追加していく
+2025-01-07 penkich
 温湿度センサー：AHT21B
 表示器　　　　：ssd1306(I2C)
-
-
-
+マイコン　　　：xiao esp32c3
 """
 from machine import Pin, I2C
 import ssd1306
@@ -22,20 +22,18 @@ oled_width = 128
 oled_height = 64
 oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
 
-
-
 def do_connect():
     import network
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect('ESSID', 'PASS')
+        sta_if.connect('ESSID', 'PASS') # 使用するESSIDとPASSを入れます。
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
 
-do_connect()
+do_connect() # WiFiに接続
 
 UTC_OFFSET = 9 * 60 * 60 # JST補正用
 ntptime.settime() # ネットワークタイム
@@ -69,7 +67,7 @@ while True:
         requests.get(f'https://script.google.com/macros/s/{deployid}/exec?yyyymmdd={yyyymmdd}&jikoku={jikoku}&temp={temperature}&humid={humidity}')
 
         # 600秒待機
-        time.sleep(600)
+        time.sleep(600) # 送信間隔（秒）を設定します。
         
 
     except Exception as e:
